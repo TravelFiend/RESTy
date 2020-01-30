96fa3672aae4fb9b659a49c40c824de0478af1a5
TravelFiend <mikegrace274@gmail.com>
2020-01-30 14:22:46 -0800

    header and footer added and styled

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <p>&copy; 2020 Mike G</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <p>RESTy</p>
+    </header>
+  );
+};
+
+export default Header;
