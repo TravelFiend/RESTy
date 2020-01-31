7188b2884b568e528dde9d7aad5f471f49250014
TravelFiend <mikegrace274@gmail.com>
2020-01-30 18:11:58 -0800

    response component in place

---
+++ src/components/response/ResponseField.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import styles from './Response.css';
+
+class ResponseField extends Component {
+
+  render() {
+    return (
+      <div className={styles.ShowResult}>
+      
+      </div>
+    );
+  }
+}
+
+export default ResponseField;
